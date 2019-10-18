d206228ceaeab86610eccf87ab32714d594b9cd8
Thomas Barr <thomas.barr1983@gmail.com>
2019-10-18 00:54:50 +0000

    first commit

--- quiz3.rb
+++ quiz3.rb
@@ -1,2 +1,14 @@
-class Superhero
+class Dog
+  attr_accessor :breed , :gender , :name , :size , :color
+  
+  def initialize(breed , gender , name , size , color)
+    @name = name
+    @breed = breed
+    @gender = gender
+    @size = size
+    @color = color
+  end  
+
 end
+
+#create instance of method with all attributes along with disply of output
